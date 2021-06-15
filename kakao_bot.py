24f821e246773071ee2df8f0c2c7f45426d5c7b9
FPast <chgy2131@naver.com>
2021-06-15 17:43:37 +0900

    test

--- kakao_bot.py
+++ kakao_bot.py
@@ -1,8 +1,8 @@
 # link : https://airfox1.tistory.com/2?category=1118519
-import time, win32con, win32api, win32gui  #pip3 install pypiwin32
-import pandas as pd #가져온 채팅내용 DF로 쓸거라서
-from module import PostKeyEx as pk
+
+import time, win32con, win32api, win32gui, ctypes #pip3 install pypiwin32
 from pywinauto import clipboard #pip3 install pywinauto , pandas
+import pandas as pd # 가져온 채팅내용 DF로 쓸거라서
 
 # # 카톡창 이름 (열려있는 상태, 최소화 X, 창뒤에 숨어있는 비활성화 상태 가능)
 kakao_opentalk_name = '채팅방이름'
@@ -12,11 +12,75 @@ chat_command = '명령'
 
 
 
+PBYTE256 = ctypes.c_ubyte * 256
+_user32 = ctypes.WinDLL("user32")
+GetKeyboardState = _user32.GetKeyboardState
+SetKeyboardState = _user32.SetKeyboardState
+PostMessage = win32api.PostMessage
+SendMessage = win32gui.SendMessage
+FindWindow = win32gui.FindWindow
+IsWindow = win32gui.IsWindow
+GetCurrentThreadId = win32api.GetCurrentThreadId
+GetWindowThreadProcessId = _user32.GetWindowThreadProcessId
+AttachThreadInput = _user32.AttachThreadInput
+
+MapVirtualKeyA = _user32.MapVirtualKeyA
+MapVirtualKeyW = _user32.MapVirtualKeyW
+
+MakeLong = win32api.MAKELONG
+w = win32con
+
+# 조합키 쓰기 위해
+def PostKeyEx(hwnd, key, shift, specialkey):
+    if IsWindow(hwnd):
+
+        ThreadId = GetWindowThreadProcessId(hwnd, None)
+
+        lparam = MakeLong(0, MapVirtualKeyA(key, 0))
+        msg_down = w.WM_KEYDOWN
+        msg_up = w.WM_KEYUP
+
+        if specialkey:
+            lparam = lparam | 0x1000000
+
+        if len(shift) > 0:  # Если есть модификаторы - используем PostMessage и AttachThreadInput
+            pKeyBuffers = PBYTE256()
+            pKeyBuffers_old = PBYTE256()
+
+            SendMessage(hwnd, w.WM_ACTIVATE, w.WA_ACTIVE, 0)
+            AttachThreadInput(GetCurrentThreadId(), ThreadId, True)
+            GetKeyboardState(ctypes.byref(pKeyBuffers_old))
+
+            for modkey in shift:
+                if modkey == w.VK_MENU:
+                    lparam = lparam | 0x20000000
+                    msg_down = w.WM_SYSKEYDOWN
+                    msg_up = w.WM_SYSKEYUP
+                pKeyBuffers[modkey] |= 128
+
+            SetKeyboardState(ctypes.byref(pKeyBuffers))
+            time.sleep(0.01)
+            PostMessage(hwnd, msg_down, key, lparam)
+            time.sleep(0.01)
+            PostMessage(hwnd, msg_up, key, lparam | 0xC0000000)
+            time.sleep(0.01)
+            SetKeyboardState(ctypes.byref(pKeyBuffers_old))
+            time.sleep(0.01)
+            AttachThreadInput(GetCurrentThreadId(), ThreadId, False)
+
+        else:  # Если нету модификаторов - используем SendMessage
+            SendMessage(hwnd, msg_down, key, lparam)
+            SendMessage(hwnd, msg_up, key, lparam | 0xC0000000)
+
+
+
+
+
 # # 채팅내용 초기 저장 _ 마지막 채팅
 def chat_last_save():
-    pk.PostKeyEx(hwndListControl, ord('A'), [w.VK_CONTROL], False)
+    PostKeyEx(hwndListControl, ord('A'), [w.VK_CONTROL], False)
     time.sleep(1)
-    pk.PostKeyEx(hwndListControl, ord('C'), [w.VK_CONTROL], False)
+    PostKeyEx(hwndListControl, ord('C'), [w.VK_CONTROL], False)
     ttext = clipboard.GetData()
 
     a = ttext.split('\r\n')   # \r\n 으로 스플릿 __ 대화내용 인용의 경우 \r 때문에 해당안됨
@@ -29,9 +93,9 @@ def chat_last_save():
 
 # # 채팅방 커멘드 체크
 def chat_chek_command(cls, clst):
-    pk.PostKeyEx(hwndListControl, ord('A'), [w.VK_CONTROL], False)
+    PostKeyEx(hwndListControl, ord('A'), [w.VK_CONTROL], False)
     time.sleep(1)
-    pk.PostKeyEx(hwndListControl, ord('C'), [w.VK_CONTROL], False)
+    PostKeyEx(hwndListControl, ord('C'), [w.VK_CONTROL], False)
     ttext = clipboard.GetData()
 
     a = ttext.split('\r\n')  # \r\n 으로 스플릿 __ 대화내용 인용의 경우 \r 때문에 해당안됨
