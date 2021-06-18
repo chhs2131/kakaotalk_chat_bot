e57272838c6b5e638d08a77a505ef13e6a42aeba
FPast <chgy2131@naver.com>
2021-06-18 13:04:17 +0900

    aplha

--- kakao_bot.py
+++ kakao_bot.py
@@ -1,21 +1,41 @@
-# link : https://airfox1.tistory.com/2?category=1118519
-
-import time, win32con, win32api, win32gui, ctypes #pip3 install pypiwin32
+#########################################################################################################
+#
+#  카카오톡 채팅봇 프로그램
+#  참조 link : https://airfox1.tistory.com/2?category=1118519
+#
+#########################################################################################################
+import time, win32con, win32api, win32gui, ctypes, configparser #pip3 install pypiwin32
 from pywinauto import clipboard #pip3 install pywinauto , pandas
 import pandas as pd #채팅내용을 DataFrame 형태로 확인
 
-# 세팅값 지정
-kakao_opentalk_name = '헌'
-window_classname = "RICHEDIT50W"
-chat_command = '명령'
+# 변수 지정
+kakaotalk_chatroom_name = ""
+refresh_cycle = 0
+window_classname = ""
+chat_command = '사랑해'
 chat_command2 = '배고파'
 
-# 전역변수 지정
 w = win32con
 
 
 
+#########################################################################################################
+#
+#  일반함수 def
+#
+#########################################################################################################
 
+# config.ini값 불러오기
+def read_config():
+    global kakaotalk_chatroom_name, refresh_cycle, window_classname
+
+    config = configparser.ConfigParser()
+    config.read('config.ini', encoding='UTF-8')
+    # 파일이 없는경우 생성?
+
+    kakaotalk_chatroom_name = config['init']['room_name']
+    refresh_cycle = config['init']['refresh_cycle']
+    window_classname = config['init']['richedit']
 
 # 조합키 사용
 PBYTE256 = ctypes.c_ubyte * 256
@@ -77,7 +97,11 @@ def PostKeyEx(hwnd, key, shift, specialkey):
 
 
 
-
+#########################################################################################################
+#
+#  채팅관련 함수 def
+#
+#########################################################################################################
 
 #지정 문구 전송
 def kakao_sendtext(text):
@@ -159,32 +183,54 @@ def open_chatroom(chatroom_name):
     SendReturn(hwndkakao_edit3)
     time.sleep(1)
 
+#채팅방 닫기
+def close_chatroom():
+    win32gui.SendMessage(hwndMain, win32con.WM_CLOSE, 0, 0)
+    time.sleep(1)
 
 
 
+#########################################################################################################
+#
+#  자동응답 관련 함수 def
+#
+#########################################################################################################
+
+# <-- 내용입력 -->
 
 
+#########################################################################################################
+#
+#  Main
+#
+#########################################################################################################
 
+#config.ini 파일 읽기
+print("read config file...")
+read_config()
+print("  RoomName : " + kakaotalk_chatroom_name)
+print("  Refresht : " + refresh_cycle)
+print("  Winclass : " + window_classname)
 
+# 채팅방 열기
+print("Open Chatroom...")
+open_chatroom(kakaotalk_chatroom_name)  
 
-
-
-open_chatroom(kakao_opentalk_name)  # 채팅방 열기
-
-# 핸들러
-hwndMain = win32gui.FindWindow( None, kakao_opentalk_name)
+# 핸들러 지정
+print("Handler Init...")
+hwndMain = win32gui.FindWindow( None, kakaotalk_chatroom_name)
 hwndEdit = win32gui.FindWindowEx( hwndMain, None, window_classname, None)
 hwndListControl = win32gui.FindWindowEx( hwndMain, None, "EVA_VH_ListControl_Dblclk", None)
+print("  Main : " + f'{hwndMain}')
+print("  Edit : " + f'{hwndEdit}')
+print("  Ctrl : " + f'{hwndListControl}')
 
-#핸들러 정보 확인
-print(hwndMain)
-print(hwndEdit)
-print(hwndListControl)
-
+print("chat init...")
 cls, clst = chat_last_save()  # 초기설정 _ 마지막채팅 저장
+
+print("run...")
 while True:
-    print(".")
     cls, clst = chat_chek_command(cls, clst)  # 커멘드 체크
-    time.sleep(5)
-    
-    win32gui.SendMessage(hwndMain, win32con.WM_CLOSE, 0, 0) #창 닫기
+    time.sleep( int(refresh_cycle) )
+
+close_chatroom()
