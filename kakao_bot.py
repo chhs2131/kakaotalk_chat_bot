2cbade43bf4421734824fd800c29796f96b963bf
FPast <chgy2131@naver.com>
2021-06-18 17:57:49 +0900

    aplha

--- kakao_bot.py
+++ kakao_bot.py
@@ -10,10 +10,19 @@ import pandas as pd #채팅내용을 DataFrame 형태로 확인
 
 # 변수 지정
 kakaotalk_chatroom_name = ""
-refresh_cycle = 0
 window_classname = ""
-chat_command = '사랑해'
-chat_command2 = '배고파'
+refresh_cycle = 0
+reopen_cycle = 0
+
+chat_command1 = ''
+chat_command2 = ''
+chat_command3 = ''
+chat_return1 = ''
+chat_return2 = ''
+chat_return3 = ''
+
+now_time = 0.0
+start_time = 0.0
 
 w = win32con
 
@@ -27,15 +36,31 @@ w = win32con
 
 # config.ini값 불러오기
 def read_config():
-    global kakaotalk_chatroom_name, refresh_cycle, window_classname
+    global kakaotalk_chatroom_name, refresh_cycle, window_classname, reopen_cycle
+    global chat_command1, chat_command2, chat_command3, chat_return1, chat_return2, chat_return3
 
     config = configparser.ConfigParser()
     config.read('config.ini', encoding='UTF-8')
     # 파일이 없는경우 생성?
 
     kakaotalk_chatroom_name = config['init']['room_name']
-    refresh_cycle = config['init']['refresh_cycle']
     window_classname = config['init']['richedit']
+    refresh_cycle = config['init']['refresh_cycle']
+    reopen_cycle = config['init']['reopen_cycle']
+    
+    chat_command1 = config['case']['command1']
+    chat_command2 = config['case']['command2']
+    chat_command3 = config['case']['command3']
+    chat_return1 = config['case']['return1']
+    chat_return2 = config['case']['return2']
+    chat_return3 = config['case']['return3']
+
+# 핸들러 값지정
+def find_hwnd():
+    global hwndMain, hwndEdit, hwndListControl
+    hwndMain = win32gui.FindWindow( None, kakaotalk_chatroom_name)
+    hwndEdit = win32gui.FindWindowEx( hwndMain, None, window_classname, None)
+    hwndListControl = win32gui.FindWindowEx( hwndMain, None, "EVA_VH_ListControl_Dblclk", None)
 
 # 조합키 사용
 PBYTE256 = ctypes.c_ubyte * 256
@@ -103,71 +128,6 @@ def PostKeyEx(hwnd, key, shift, specialkey):
 #
 #########################################################################################################
 
-#지정 문구 전송
-def kakao_sendtext(text):
-    win32api.SendMessage(hwndEdit, win32con.WM_SETTEXT, 0, text)
-    SendReturn(hwndEdit)
-
-#엔터
-def SendReturn(hwnd):
-    win32api.PostMessage(hwnd, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
-    time.sleep(0.01)
-    win32api.PostMessage(hwnd, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
-
-#채팅내용 복사
-def kakao_copy():
-    PostKeyEx(hwndListControl, ord('A'), [w.VK_CONTROL], False)
-    time.sleep(1)
-    PostKeyEx(hwndListControl, ord('C'), [w.VK_CONTROL], False)
-    ttext = clipboard.GetData()
-    return ttext
-
-
-#채팅방 내용 저장 - 초기값 지정
-def chat_last_save():
-    ttext = kakao_copy()
-    a = ttext.split('\r\n')   # \r\n 으로 스플릿 __ 대화내용 인용의 경우 \r 때문에 해당안됨
-    df = pd.DataFrame(a)    # DF 으로 바꾸기
-
-    df[0] = df[0].str.replace('\[([\S\s]+)\] \[(오전|오후)([0-9:\s]+)\] ', '')  # 정규식으로 채팅내용만 남기기
-    return df.index[-2], df.iloc[-2, 0]
-
-#채팅방 내용 저장 - 주기적 확인
-def chat_chek_command(cls, clst):
-    ttext = kakao_copy()
-    a = ttext.split('\r\n')  # \r\n 으로 스플릿 __ 대화내용 인용의 경우 \r 때문에 해당안됨
-    df = pd.DataFrame(a)  # DF 으로 바꾸기
-
-    df[0] = df[0].str.replace('\[([\S\s]+)\] \[(오전|오후)([0-9:\s]+)\] ', '')  # 정규식으로 채팅내용만 남기기
-
-    if df.iloc[-2, 0] == clst:
-        print("신규채팅 없음")
-        return df.index[-2], df.iloc[-2, 0]
-    else:
-        print("신규채팅 발생")
-        df1 = df.iloc[cls+1 : , 0]   # 최근 채팅내용만 남김
-
-        found = df1[ df1.str.contains(chat_command) ]    # 명령어 여부 카운트
-        found2 = df1[ df1.str.contains(chat_command2) ]    # 명령어 여부 카운트
-
-        if 1 <= int(found.count()):
-            print("<--[커맨드 확인]-->")
-            kakao_sendtext("삐빅! 감지되었습니다.")  # 메시지 전송,, 추후 함수로 분리하여 처리
-
-            # 명령어 여러개 쓸경우 리턴값으로 각각 빼서 쓰면 될듯. 일단 테스트용으로 위에 하드코딩 해둠
-            return df.index[-2], df.iloc[-2, 0]
-
-        if 1 <= int(found2.count()):
-            print("<--[커맨드 확인]-->")
-            kakao_sendtext("삐빅! 자동응답기. 밥머그에숑")  # 메시지 전송,, 추후 함수로 분리하여 처리
-
-            # 명령어 여러개 쓸경우 리턴값으로 각각 빼서 쓰면 될듯. 일단 테스트용으로 위에 하드코딩 해둠
-            return df.index[-2], df.iloc[-2, 0]
-
-        else:
-            print("<--[커맨드 없음]-->")
-            return df.index[-2], df.iloc[-2, 0]
-
 #채팅방 열기
 def open_chatroom(chatroom_name):
     # # 채팅방 목록 검색하는 Edit (채팅방이 열려있지 않아도 전송 가능하기 위하여)
@@ -188,15 +148,88 @@ def close_chatroom():
     win32gui.SendMessage(hwndMain, win32con.WM_CLOSE, 0, 0)
     time.sleep(1)
 
+#지정 문구 전송
+def kakao_sendtext(text):
+    win32api.SendMessage(hwndEdit, win32con.WM_SETTEXT, 0, text)
+    SendReturn(hwndEdit)
+
+#엔터
+def SendReturn(hwnd):
+    win32api.PostMessage(hwnd, win32con.WM_KEYDOWN, win32con.VK_RETURN, 0)
+    time.sleep(0.01)
+    win32api.PostMessage(hwnd, win32con.WM_KEYUP, win32con.VK_RETURN, 0)
+
+#채팅내용 복사
+def kakao_copy():
+    PostKeyEx(hwndListControl, ord('A'), [w.VK_CONTROL], False)
+    time.sleep(1)
+    PostKeyEx(hwndListControl, ord('C'), [w.VK_CONTROL], False)
+    ttext = clipboard.GetData()
+    return ttext
+
+
+#초기 채팅방 대화내역 확인
+def chat_last_save():
+    ttext = kakao_copy()
+    a = ttext.split('\r\n')   # \r\n 으로 스플릿 __ 대화내용 인용의 경우 \r 때문에 해당안됨
+    df = pd.DataFrame(a)    # DF 으로 바꾸기
+
+    df[0] = df[0].str.replace('\[([\S\s]+)\] \[(오전|오후)([0-9:\s]+)\] ', '')  # 정규식으로 채팅내용만 남기기
+    return df.index[-2], df.iloc[-2, 0]
+
+#신규 대화내역 존재 확인
+def chat_chek_command(cls, clst):
+    ttext = kakao_copy()
+    a = ttext.split('\r\n')  # \r\n 으로 스플릿 __ 대화내용 인용의 경우 \r 때문에 해당안됨
+    df = pd.DataFrame(a)  # DF 으로 바꾸기
+
+    df[0] = df[0].str.replace('\[([\S\s]+)\] \[(오전|오후)([0-9:\s]+)\] ', '')  # 정규식으로 채팅내용만 남기기
+
+    if df.iloc[-2, 0] != clst: #신규채팅이 있는 경우
+        print("신규채팅 발생")
+        df1 = df.iloc[cls+1 : , 0]   # 최근 채팅내용만 남김
+
+        found = df1[ df1.str.contains(chat_command1) ]    # 명령어 여부 카운트  # str.find
+        found2 = df1[ df1.str.contains(chat_command2) ]    # 명령어 여부 카운트
+
+        if 1 <= int(found.count()):
+            bot_return1()
+        if 1 <= int(found2.count()):
+            bot_return2()
+
+    return df.index[-2], df.iloc[-2, 0]
 
 
 #########################################################################################################
 #
-#  자동응답 관련 함수 def
+#  커맨드 응답 관련 함수 def
 #
 #########################################################################################################
 
-# <-- 내용입력 -->
+# 종목에 대한 간단 설명
+
+def bot_return1():
+    print("커맨드 확인")
+    kakao_sendtext(chat_return1)  # 메시지 전송,, 추후 함수로 분리하여 처리
+
+def bot_return2():
+    print("커맨드 확인")
+    kakao_sendtext(chat_return2)  # 메시지 전송,, 추후 함수로 분리하여 처리
+
+def bot_return3():
+    print("커맨드 확인")
+    kakao_sendtext(chat_return3)  # 메시지 전송,, 추후 함수로 분리하여 처리
+
+
+
+#########################################################################################################
+#
+#  스케쥴(cron) 관련 함수 def
+#
+#########################################################################################################
+
+# <-- 내용입력 --> , 매 n시 뉴스 헤드라인 rss ,, 매일 언급 종목 분석 ,, 매일 대화량 순위분석 ,, 긴급포착 ,, 금일 장마감 (코스피,코스닥 량)
+
 
 
 #########################################################################################################
@@ -206,31 +239,48 @@ def close_chatroom():
 #########################################################################################################
 
 #config.ini 파일 읽기
-print("read config file...")
+print("[read config file...]")
 read_config()
 print("  RoomName : " + kakaotalk_chatroom_name)
-print("  Refresht : " + refresh_cycle)
 print("  Winclass : " + window_classname)
+print("  Refresht : " + refresh_cycle)
+print("  Reopency : " + reopen_cycle)
+print("  " + chat_command1 + " / " + chat_return1)
+print("  " + chat_command2 + " / " + chat_return2)
+print("  " + chat_command3 + " / " + chat_return3)
 
 # 채팅방 열기
-print("Open Chatroom...")
+print("[Open Chatroom...]")
 open_chatroom(kakaotalk_chatroom_name)  
 
 # 핸들러 지정
-print("Handler Init...")
+print("[Handler Init...]")
 hwndMain = win32gui.FindWindow( None, kakaotalk_chatroom_name)
 hwndEdit = win32gui.FindWindowEx( hwndMain, None, window_classname, None)
 hwndListControl = win32gui.FindWindowEx( hwndMain, None, "EVA_VH_ListControl_Dblclk", None)
+find_hwnd()
 print("  Main : " + f'{hwndMain}')
 print("  Edit : " + f'{hwndEdit}')
 print("  Ctrl : " + f'{hwndListControl}')
 
-print("chat init...")
+print("[chat init...]")
 cls, clst = chat_last_save()  # 초기설정 _ 마지막채팅 저장
 
-print("run...")
+print("[run...]")
+start_time = time.time()
 while True:
     cls, clst = chat_chek_command(cls, clst)  # 커멘드 체크
+    now_time = time.time()
+    
+    print("  채팅방 재실행 주기 : {0: 5.2f} / {1}".format(now_time - start_time, reopen_cycle))
+    if now_time - start_time > int(reopen_cycle):
+        print("대화방 재실행")
+        close_chatroom()
+        time.sleep(1)
+        open_chatroom(kakaotalk_chatroom_name) 
+        time.sleep(1)
+        find_hwnd()
+        time.sleep(1)
+
     time.sleep( int(refresh_cycle) )
 
-close_chatroom()
